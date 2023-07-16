f223a693d4f93eed64d161808313dc41030872fa
Bikash kumar <kbkash845@gmail.com>
2023-07-16 20:27:15 +0530

    Commit

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import classes from './AvailableMeals.module.css'
+import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItems/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -30,12 +32,17 @@ const DUMMY_MEALS = [
 
 
 function AvailableMeals() {
-    const MealList=DUMMY_MEALS.map((meal)=><li>{meal.name}</li>)
+    const MealList=DUMMY_MEALS.map((meal)=><MealItem 
+    key={meal.id}
+    name={meal.name} 
+    description={meal.description}
+    price={meal.price}
+    />)
   return (
    <section className={classes.meals}>
-   <ul>
+   <Card>
     {MealList}
-   </ul>
+   </Card>
    </section>
   )
 }
